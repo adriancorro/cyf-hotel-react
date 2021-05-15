0e7a6f67e76fcd1617a926cdbb15240910e0ebc5
adriancorro <adriancorrog@gmail.com>
2021-05-16 00:58:31 +0200

    en desarrollo

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchResults from "./SearchResults.js";
 import Search from "./Search.js";
 import SearchResultsFetcher from "./SearchResultsFetcher.js";
 
 const Bookings = () => {
-  const search2 = searchVal => {
+  const [searchInput, setSearchInput] = useState(false);
+
+  const Search2 = searchVal => {
     console.info("TO DO!", searchVal);
+    console.log(
+      searchVal.charAt(0).toUpperCase() + searchVal.slice(1).toLowerCase()
+    );
+    setSearchInput(
+      searchVal.charAt(0).toUpperCase() + searchVal.slice(1).toLowerCase()
+    );
   };
+
+  console.log(searchInput);
 
   return (
     <div className="App-content">
       <div className="container">
-        <Search searchKey={search2} />
-        <SearchResults results={SearchResultsFetcher()} />
-        <SearchResults
-          results={SearchResultsFetcher().filter(e => e.title === "Doctor")}
-        />
+        <Search searchKey={Search2} />
+
+        {!searchInput ? (
+          <SearchResults results={SearchResultsFetcher()} />
+        ) : (
+          <SearchResults
+            results={SearchResultsFetcher().filter(
+              e =>
+                e.title === searchInput ||
+                e.firstName === searchInput ||
+                e.surname === searchInput ||
+                e.id == searchInput
+            )}
+          />
+        )}
       </div>
     </div>
   );
