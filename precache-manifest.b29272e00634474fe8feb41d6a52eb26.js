749478d490487c54f14481b41ff914c8b882f0c6
adriancorro <adriancorrog@gmail.com>
2021-05-20 02:01:33 +0200

    Updates

---
+++ precache-manifest.b29272e00634474fe8feb41d6a52eb26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ccd4e9e457c52a3a728e5e2b77ce08",
+    "url": "/cyf-hotel-react/index.html"
+  },
+  {
+    "revision": "a5b26073c1dc629463a4",
+    "url": "/cyf-hotel-react/static/css/main.24586c24.chunk.css"
+  },
+  {
+    "revision": "0b24a16546977a28f2df",
+    "url": "/cyf-hotel-react/static/js/2.ae78182f.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/cyf-hotel-react/static/js/2.ae78182f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b26073c1dc629463a4",
+    "url": "/cyf-hotel-react/static/js/main.61c70e6d.chunk.js"
+  },
+  {
+    "revision": "a17eedfa60f26189a33f",
+    "url": "/cyf-hotel-react/static/js/runtime-main.72cf73f6.js"
+  }
+]);
